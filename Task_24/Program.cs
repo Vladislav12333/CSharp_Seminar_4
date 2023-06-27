98b13d48087f84a5b4ee48064de923496bd529bf
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-06-27 19:59:27 +0300

    Написали программу (Task_24) 2  вариант решения задачи

--- Task_24/Program.cs
+++ Task_24/Program.cs
@@ -3,13 +3,23 @@
 // 8 -> 10
 // 4 -> 36
 
-System.Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-
-for (int i = 1; i <= number; i++)
+int ReadInt(string text)
 {
-    sum = sum + i;
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
+int SumNumbers(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+//---------------------------------------------------------------
 
-System.Console.WriteLine($"Сумма чисел от 1 дл {number}: {sum}");
+int number = ReadInt("Введите число: ");
+int sum = SumNumbers(number);
+
+System.Console.WriteLine($"Сумма чисел от 1 до {number}: {sum}");
