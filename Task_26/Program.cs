aff707edfad2e948df6563bd5879d58ef2879387
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-07-07 18:39:40 +0300

    Написали программу (Task_26)

---
+++ Task_26/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 26. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+//Решение задачис помощью функции
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+int CountNumbers(int N)
+{
+    int count = 0;
+    if (N == 0) count = 1;
+    while (N > 0)
+    {
+        N /= 10;
+        count++;
+    }
+    return count;
+}
+
+// ----------------------------------------------------------------------------------
+
+int count = CountNumbers(ReadInt("Введите число: "));
+System.Console.WriteLine($"Количество цифр в числе -> {count}");
